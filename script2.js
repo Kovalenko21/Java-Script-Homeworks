760b755497412562ee66091f54b32e7678aad6a4
Anatoly Kalyuzhny <tolibuster21@gmail.com>
2021-05-19 12:54:21 +0300

    Fix mistake in 4th task

--- script2.js
+++ script2.js
@@ -49,9 +49,9 @@ if (
 } else alert("FALSE");
 
 let year = prompt("Enter year you want to check");
-if (year % 4 == 0 && year % 400 == 0) {
+if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
   alert("This is a leap year");
-} else if (year % 100 != 0) {
+} else {
   alert("This is not a leap year");
 }
 
